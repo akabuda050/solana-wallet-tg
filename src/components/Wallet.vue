fd1f9ed494c96f4921d37c0cbd893b5e2524b956
Olexandr Dolbin <olexandr.dolbin@gmail.com>
2023-05-21 03:36:59 +0200

    add wallet

--- src/components/Wallet.vue
+++ src/components/Wallet.vue
@@ -1,9 +1,25 @@
 <template>
-    <div>
-        Wallet
-        Login|Register
+    <div class="flex flex-col items-center">
+        <h1>Wallet</h1>
+        <span>Login|Register</span>
     </div>
 </template>
 <script setup lang="ts">
 </script>
-<style scoped></style>
+<style scoped>
+.flex {
+    display: flex;
+}
+
+.flex-col {
+    flex-direction: column;
+}
+
+.items-center {
+    align-items: center;
+}
+
+.justify-center {
+    justify-content: center;
+}
+</style>
