1b3b36f78e8bd87ec27075faceef9a1c5a2b6fef
Olexandr Dolbin <olexandr.dolbin@gmail.com>
2023-05-21 04:46:25 +0200

    fixes

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 // @ts-nocheck
 import WalletVue from './components/Wallet.vue';
+import {WebView, WebApp} from './scripts/tg';
+WebView();
+WebApp();
 
 const telegram = window.Telegram.WebApp
 telegram.ready()
